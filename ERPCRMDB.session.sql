14c3601d2cdb2b32cfb3da84ca2ff38585c9c855
hari3mo <harisasaif@gmail.com>
2024-07-15 05:21:30 -0700

    db

---
+++ ERPCRMDB.session.sql
@@ -0,0 +1,27 @@
+--@block
+ALTER TABLE Accounts
+ADD PRIMARY KEY (AccountID)
+
+--@block
+ALTER TABLE Leads
+ADD PRIMARY KEY (LeadID),
+ADD FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+
+--@block
+ALTER TABLE Orders
+ADD PRIMARY KEY (OrderID),
+ADD FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID);
+
+--@block
+ALTER TABLE OrderDetails
+ADD PRIMARY KEY (OrderDetailID),
+ADD FOREIGN KEY (OrderID) REFERENCES Orders(OrderID);
+
+--@block
+ALTER TABLE SupportTickets
+ADD PRIMARY KEY (TicketID),
+ADD FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID),
+ADD FOREIGN KEY (ContactID) REFERENCES Contacts(ContactID)
+
+
+--@block
