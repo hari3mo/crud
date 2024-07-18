5c6dd6d95a9ffa6ec3d1d8c9408112f626616844
hari3mo <harisasaif@gmail.com>
2024-07-17 19:09:27 -0700

    db

---
+++ flask/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/leads/')
+def leads():
+    return render_template("leads.html")
+
+
+@app.route('/accounts/')
+def accounts():
+    return render_template("accounts.html")
+
+@app.route('/test/')
+def test():
+    return render_template("py.html")
+    
+
+
+# @app.route('/<loop>/')
+# def test(loop):
+#         return f"Hello {loop}"
+    
+# @app.route('/admin/')
+# def admin():
+#     return redirect(url_for("test", loop='hello'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
