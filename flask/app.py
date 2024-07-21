7caf9efc6443d177bf3a6de09f5629fe827979f8
hari3mo <harisasaif@gmail.com>
2024-07-21 15:25:48 -0700

    db

--- flask/app.py
+++ flask/app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-url_for('static', filename='style.css')
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/leads/')
-def leads():
-    return render_template("leads.html")
+# @app.route('/leads/')
+# def leads():
+#     return render_template("leads.html")
 
 
-@app.route('/accounts/')
-def accounts():
-    return render_template("accounts.html")
+# @app.route('/accounts/')
+# def accounts():
+#     return render_template("accounts.html")
 
-@app.route('/test/')
-def test():
-    return render_template("py.html")
+# @app.route('/test/')
+# def test():
+#     return render_template("py.html")
     
 
 
