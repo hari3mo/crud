61c9b4cc6f05d18512cdbc2e6c1e6e0aa1f078bf
hari3mo <harisasaif@gmail.com>
2024-07-21 16:54:36 -0700

    db

--- flask/app.py
+++ flask/app.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-# @app.route('/leads/')
-# def leads():
-#     return render_template("leads.html")
+    return render_template('index.html')
 
 
-# @app.route('/accounts/')
-# def accounts():
-#     return render_template("accounts.html")
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST': 
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else: 
+        return render_template('login.html')
+    
+
+@app.route("/<usr>")
+def user(usr):
+    return f'<h1>{usr}</h1>'
+
 
 # @app.route('/test/')
 # def test():
 #     return render_template("py.html")
     
-
 
 # @app.route('/<loop>/')
 # def test(loop):
