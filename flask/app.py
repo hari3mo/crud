cb08ebb5fbc962a29b15f512959289e6a9078b69
hari3mo <harisasaif@gmail.com>
2024-07-21 18:49:57 -0700

    db

--- flask/app.py
+++ flask/app.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 
+app.secret_key = 'key'
+app.permanent_session_lifetime = timedelta(minutes=60)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST': 
         user = request.form['nm']
-        return redirect(url_for("user", usr=user))
-    else: 
+        session['user'] = user
+        return redirect(url_for('index'))
+    else:
+        if 'user' in session:
+            return redirect(url_for('index'))
         return render_template('login.html')
-    
 
-@app.route("/<usr>")
-def user(usr):
-    return f'<h1>{usr}</h1>'
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        usr = session['user']
+        return f'<h1>{usr}</h1>'
+    else:
+        return redirect(url_for('login'))
 
 
 # @app.route('/test/')
