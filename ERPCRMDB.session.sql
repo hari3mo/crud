365685c1066c5948f60b60a45e9b6ebf7827f268
hari3mo <harisasaif@gmail.com>
2024-08-17 01:24:58 -0700

    overall style + image rename

--- ERPCRMDB.session.sql
+++ ERPCRMDB.session.sql
@@ -58,3 +58,11 @@ INSERT INTO Clients VALUES (100100, 'Southern California Edison', '9b2a012a1a1c4
 --@block
 INSERT INTO Clients VALUES(100000, 'ERP Center, Inc.', '3a95d70c85864ab99603', '/images/3a95d70c85864ab99603.png', 1, 1)
 
+--@block
+ALTER TABLE Leads
+ADD PRIMARY KEY (LeadID)
+
+--@block
+UPDATE Clients
+SET `Image` = '/images/100010.png'
+WHERE (ClientID=100010);
